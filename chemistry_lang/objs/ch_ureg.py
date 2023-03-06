837b0759664ea99a248b1c50a0aeb213a9a41a98
Yubo-Cao <cao2006721@gmail.com>
2023-03-05 22:06:22 -0500

    fix: filp division relationship of avogadro's number and mole

--- chemistry_lang/objs/ch_ureg.py
+++ chemistry_lang/objs/ch_ureg.py
@@ -2,5 +2,5 @@ import pint
 from pint import UnitRegistry
 
 ureg = UnitRegistry()
-ureg.define("atom = 6.02e23 * mole")
+ureg.define("atom = mole / 6.0221408e+23")
 pint.set_application_registry(ureg)
